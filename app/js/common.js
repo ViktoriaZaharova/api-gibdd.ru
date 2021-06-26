3c1ad151e78e115d6d59df5219423b2340e7f697
Viktoria <zaharova181114@gmail.com>
2021-06-26 21:49:33 +0300

    edit1

--- app/js/common.js
+++ app/js/common.js
@@ -14,8 +14,22 @@ $('.menu li a').each(function () {
         $(this).addClass('active');
     }
 });
+
+$('.menu-sidebar li a').each(function () {
+    let location = window.location.href;
+    let link = this.href;
+    if (location === link) {
+        $(this).addClass('active');
+    }
+});
 // end
 
+$('.btn-sidebar').on('click', function () {
+   $('.main-sidebar').fadeToggle();
+});
 
+$('.sidebar-close').on('click', function () {
+    $('.main-sidebar').fadeOut();
+});
 
 $('[data-toggle="tooltip"]').tooltip();
